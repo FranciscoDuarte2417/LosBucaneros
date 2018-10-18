2b3ae2e37975b1e0b2ef3d5cad4721f002aff382
Administrador <Administrador@LOSBUCANEROS>
2018-10-18 14:05:11 -0700

    Z

--- LosBucanerosApp/LosBucanerosApp/Form1.Designer.cs
+++ LosBucanerosApp/LosBucanerosApp/Form1.Designer.cs
@@ -144,7 +144,7 @@
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(176, 31);
             this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Ingresar al Sistema 44";
+            this.btnLogin.Text = "Ingresar al Sistema 55";
             this.btnLogin.UseVisualStyleBackColor = true;
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
