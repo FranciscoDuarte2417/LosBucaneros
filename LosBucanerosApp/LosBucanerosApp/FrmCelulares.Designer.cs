59a0b0ee8fa491581cc55e5119e514ea7a243798
Administrador <Administrador@LOSBUCANEROS>
2018-10-18 14:11:21 -0700

    Cambio texto

--- LosBucanerosApp/LosBucanerosApp/FrmCelulares.Designer.cs
+++ LosBucanerosApp/LosBucanerosApp/FrmCelulares.Designer.cs
@@ -241,7 +241,7 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.MidnightBlue;
-            this.button2.Font = new System.Drawing.Font("NewsGoth Lt BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.button2.Location = new System.Drawing.Point(0, 252);
@@ -255,7 +255,7 @@
             // btnCelulares
             // 
             this.btnCelulares.BackColor = System.Drawing.Color.MidnightBlue;
-            this.btnCelulares.Font = new System.Drawing.Font("NewsGoth Lt BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCelulares.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCelulares.ForeColor = System.Drawing.Color.White;
             this.btnCelulares.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.btnCelulares.Location = new System.Drawing.Point(0, 186);
@@ -300,7 +300,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.MidnightBlue;
-            this.button1.Font = new System.Drawing.Font("NewsGoth Lt BT", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.button1.Location = new System.Drawing.Point(0, 324);
@@ -344,9 +344,9 @@
             this.panel4.Controls.Add(this.label7);
             this.panel4.Controls.Add(this.panel5);
             this.panel4.Controls.Add(this.label9);
-            this.panel4.Location = new System.Drawing.Point(296, 40);
+            this.panel4.Location = new System.Drawing.Point(54, 169);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(207, 126);
+            this.panel4.Size = new System.Drawing.Size(279, 126);
             this.panel4.TabIndex = 13;
             // 
             // label7
@@ -355,7 +355,7 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Red;
-            this.label7.Location = new System.Drawing.Point(56, 65);
+            this.label7.Location = new System.Drawing.Point(92, 65);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(150, 24);
             this.label7.TabIndex = 15;
@@ -370,7 +370,7 @@
             this.panel5.Controls.Add(this.label8);
             this.panel5.Location = new System.Drawing.Point(-1, -1);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(207, 32);
+            this.panel5.Size = new System.Drawing.Size(279, 32);
             this.panel5.TabIndex = 13;
             // 
             // label8
@@ -379,11 +379,11 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(15, 2);
+            this.label8.Location = new System.Drawing.Point(3, 2);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(161, 29);
+            this.label8.Size = new System.Drawing.Size(254, 29);
             this.label8.TabIndex = 3;
-            this.label8.Text = "Responsivas";
+            this.label8.Text = "Responsivas Activas";
             // 
             // label9
             // 
@@ -391,7 +391,7 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(55, 36);
+            this.label9.Location = new System.Drawing.Point(89, 34);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(118, 24);
             this.label9.TabIndex = 4;
