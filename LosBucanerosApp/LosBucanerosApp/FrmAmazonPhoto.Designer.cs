46afb1917149ce73ab461436e1437b0398e2a53e
Administrador <Administrador@LOSBUCANEROS>
2018-10-19 14:46:08 -0700

    Agregar responsiva firmada

---
+++ LosBucanerosApp/LosBucanerosApp/FrmAmazonPhoto.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace LosBucanerosApp
+{
+    partial class FrmAmazonPhoto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.txtpath = new System.Windows.Forms.TextBox();
+            this.btnsubir = new System.Windows.Forms.Button();
+            this.Archivo = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(12, 12);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 0;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // txtpath
+            // 
+            this.txtpath.Location = new System.Drawing.Point(93, 14);
+            this.txtpath.Name = "txtpath";
+            this.txtpath.Size = new System.Drawing.Size(364, 20);
+            this.txtpath.TabIndex = 1;
+            // 
+            // btnsubir
+            // 
+            this.btnsubir.Location = new System.Drawing.Point(382, 62);
+            this.btnsubir.Name = "btnsubir";
+            this.btnsubir.Size = new System.Drawing.Size(75, 23);
+            this.btnsubir.TabIndex = 2;
+            this.btnsubir.Text = "Subir";
+            this.btnsubir.UseVisualStyleBackColor = true;
+            this.btnsubir.Click += new System.EventHandler(this.btnsubir_Click);
+            // 
+            // Archivo
+            // 
+            this.Archivo.FileName = "openFileDialog1";
+            // 
+            // FrmAmazonPhoto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 97);
+            this.Controls.Add(this.btnsubir);
+            this.Controls.Add(this.txtpath);
+            this.Controls.Add(this.btnbuscar);
+            this.Name = "FrmAmazonPhoto";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Subir foto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.TextBox txtpath;
+        private System.Windows.Forms.Button btnsubir;
+        private System.Windows.Forms.OpenFileDialog Archivo;
+    }
+}
---
+++ LosBucanerosApp/LosBucanerosApp/FrmAmazonPhoto.cs
@@ -0,0 +1,68 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LosBucanerosApp
+{
+    public partial class FrmAmazonPhoto : Form
+    {
+        public FrmAmazonPhoto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            Archivo = new OpenFileDialog();
+            DialogResult result = Archivo.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtpath.Text = Archivo.FileName;
+            }
+        }
+
+        private void btnsubir_Click(object sender, EventArgs e)
+        {
+            UploadFileToS3(txtpath.Text,"ResponsivasAdministrativos");
+        }
+        private void UploadFileToS3(string filePath, string subDirectoryInBucket)
+        {
+            var awsAccessKey = "AKIAI5KHC3XKCDKYP3NQ";
+            var awsSecretKey = "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";
+            var existingBucketName = "losbucaneros";
+            var client = Amazon.AWSClientFactory.CreateAmazonS3Client(awsAccessKey, awsSecretKey, RegionEndpoint.USEast1);
+
+            var uploadRequest = new TransferUtilityUploadRequest
+            {
+                FilePath = filePath,
+                BucketName = existingBucketName,
+                CannedACL = S3CannedACL.PublicRead
+            };
+
+            var fileTransferUtility = new TransferUtility(client);
+            TransferUtilityUploadRequest request = new TransferUtilityUploadRequest();
+            if (subDirectoryInBucket == "" || subDirectoryInBucket == null)
+            {
+                request.BucketName = existingBucketName; //no subdirectory just bucket name
+            }
+            else
+            {   // subdirectory and bucket name
+                request.BucketName = existingBucketName + @"/" + subDirectoryInBucket;
+            }
+            //request.Key = fileNameInS3; //file name up in S3
+            request.FilePath = filePath; //local file name
+            fileTransferUtility.Upload(request); //commensing the transfer
+
+            //  fileTransferUtility.Upload(uploadRequest);
+        }
+    }
+}
