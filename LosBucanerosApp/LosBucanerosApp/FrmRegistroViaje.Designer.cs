7c493cfe4819732440189fef45b7ec9561558664
Francisco Duarte <Francisco Duarte@LAP-FDUARTE>
2018-11-05 18:23:06 -0700

    SPTM

--- LosBucanerosApp/LosBucanerosApp/FrmRegistroViaje.Designer.cs
+++ LosBucanerosApp/LosBucanerosApp/FrmRegistroViaje.Designer.cs
@@ -71,6 +71,10 @@
             this.label20 = new System.Windows.Forms.Label();
             this.label21 = new System.Windows.Forms.Label();
             this.chkOperadorPendiente = new System.Windows.Forms.CheckBox();
+            this.label22 = new System.Windows.Forms.Label();
+            this.txtDireccionCarga = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label23 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbltipoviaje
@@ -164,7 +168,7 @@
             this.txtProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.txtProducto.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.txtProducto.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtProducto.Location = new System.Drawing.Point(393, 262);
+            this.txtProducto.Location = new System.Drawing.Point(393, 353);
             this.txtProducto.Name = "txtProducto";
             this.txtProducto.Size = new System.Drawing.Size(194, 25);
             this.txtProducto.TabIndex = 9;
@@ -173,7 +177,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(323, 267);
+            this.label5.Location = new System.Drawing.Point(323, 358);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 20);
             this.label5.TabIndex = 11;
@@ -194,7 +198,7 @@
             this.txtTemperatura.Enabled = false;
             this.txtTemperatura.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.txtTemperatura.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtTemperatura.Location = new System.Drawing.Point(103, 308);
+            this.txtTemperatura.Location = new System.Drawing.Point(103, 399);
             this.txtTemperatura.Name = "txtTemperatura";
             this.txtTemperatura.Size = new System.Drawing.Size(194, 25);
             this.txtTemperatura.TabIndex = 10;
@@ -203,7 +207,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(8, 311);
+            this.label7.Location = new System.Drawing.Point(8, 402);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(85, 20);
             this.label7.TabIndex = 15;
@@ -213,7 +217,7 @@
             // 
             this.rbCentigrados.AutoSize = true;
             this.rbCentigrados.Enabled = false;
-            this.rbCentigrados.Location = new System.Drawing.Point(307, 300);
+            this.rbCentigrados.Location = new System.Drawing.Point(307, 391);
             this.rbCentigrados.Name = "rbCentigrados";
             this.rbCentigrados.Size = new System.Drawing.Size(36, 17);
             this.rbCentigrados.TabIndex = 11;
@@ -225,7 +229,7 @@
             // 
             this.rbFarenheit.AutoSize = true;
             this.rbFarenheit.Enabled = false;
-            this.rbFarenheit.Location = new System.Drawing.Point(307, 323);
+            this.rbFarenheit.Location = new System.Drawing.Point(307, 414);
             this.rbFarenheit.Name = "rbFarenheit";
             this.rbFarenheit.Size = new System.Drawing.Size(35, 17);
             this.rbFarenheit.TabIndex = 12;
@@ -240,7 +244,7 @@
             this.txtRangoTemperaturas.Enabled = false;
             this.txtRangoTemperaturas.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.txtRangoTemperaturas.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtRangoTemperaturas.Location = new System.Drawing.Point(423, 308);
+            this.txtRangoTemperaturas.Location = new System.Drawing.Point(423, 399);
             this.txtRangoTemperaturas.Name = "txtRangoTemperaturas";
             this.txtRangoTemperaturas.Size = new System.Drawing.Size(164, 25);
             this.txtRangoTemperaturas.TabIndex = 13;
@@ -249,7 +253,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(351, 311);
+            this.label8.Location = new System.Drawing.Point(351, 402);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(66, 20);
             this.label8.TabIndex = 19;
@@ -264,7 +268,7 @@
             this.cmbTipoCarga.Items.AddRange(new object[] {
             "REFRIGERADO",
             "SECO"});
-            this.cmbTipoCarga.Location = new System.Drawing.Point(104, 259);
+            this.cmbTipoCarga.Location = new System.Drawing.Point(104, 350);
             this.cmbTipoCarga.Name = "cmbTipoCarga";
             this.cmbTipoCarga.Size = new System.Drawing.Size(194, 28);
             this.cmbTipoCarga.TabIndex = 8;
@@ -274,7 +278,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(2, 267);
+            this.label9.Location = new System.Drawing.Point(2, 358);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(92, 20);
             this.label9.TabIndex = 21;
@@ -284,7 +288,7 @@
             // 
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label6.Font = new System.Drawing.Font("Arial Narrow", 1F);
-            this.label6.Location = new System.Drawing.Point(-43, 246);
+            this.label6.Location = new System.Drawing.Point(-43, 337);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(710, 10);
             this.label6.TabIndex = 23;
@@ -293,7 +297,7 @@
             // 
             this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label10.Font = new System.Drawing.Font("Arial Narrow", 1F);
-            this.label10.Location = new System.Drawing.Point(-43, 356);
+            this.label10.Location = new System.Drawing.Point(-43, 447);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(710, 10);
             this.label10.TabIndex = 24;
@@ -302,7 +306,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(29, 394);
+            this.label11.Location = new System.Drawing.Point(29, 485);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(65, 20);
             this.label11.TabIndex = 25;
@@ -314,7 +318,7 @@
             this.txtTracto.Enabled = false;
             this.txtTracto.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.txtTracto.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtTracto.Location = new System.Drawing.Point(393, 389);
+            this.txtTracto.Location = new System.Drawing.Point(393, 480);
             this.txtTracto.Name = "txtTracto";
             this.txtTracto.Size = new System.Drawing.Size(194, 25);
             this.txtTracto.TabIndex = 16;
@@ -323,7 +327,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(333, 394);
+            this.label12.Location = new System.Drawing.Point(333, 485);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(49, 20);
             this.label12.TabIndex = 27;
@@ -336,7 +340,7 @@
             this.cmbOperador.BackColor = System.Drawing.Color.White;
             this.cmbOperador.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.cmbOperador.FormattingEnabled = true;
-            this.cmbOperador.Location = new System.Drawing.Point(104, 386);
+            this.cmbOperador.Location = new System.Drawing.Point(104, 477);
             this.cmbOperador.Name = "cmbOperador";
             this.cmbOperador.Size = new System.Drawing.Size(194, 28);
             this.cmbOperador.TabIndex = 15;
@@ -347,7 +351,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(54, 209);
+            this.label13.Location = new System.Drawing.Point(54, 300);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(36, 20);
             this.label13.TabIndex = 30;
@@ -358,7 +362,7 @@
             this.dtpHora.Checked = false;
             this.dtpHora.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.dtpHora.Format = System.Windows.Forms.DateTimePickerFormat.Time;
-            this.dtpHora.Location = new System.Drawing.Point(398, 206);
+            this.dtpHora.Location = new System.Drawing.Point(398, 297);
             this.dtpHora.Name = "dtpHora";
             this.dtpHora.ShowUpDown = true;
             this.dtpHora.Size = new System.Drawing.Size(105, 25);
@@ -367,7 +371,7 @@
             // dtpFecha
             // 
             this.dtpFecha.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
-            this.dtpFecha.Location = new System.Drawing.Point(104, 205);
+            this.dtpFecha.Location = new System.Drawing.Point(104, 296);
             this.dtpFecha.Name = "dtpFecha";
             this.dtpFecha.Size = new System.Drawing.Size(237, 25);
             this.dtpFecha.TabIndex = 6;
@@ -376,7 +380,7 @@
             // 
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(351, 210);
+            this.label14.Location = new System.Drawing.Point(351, 301);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(41, 20);
             this.label14.TabIndex = 33;
@@ -387,7 +391,7 @@
             this.txtsello.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.txtsello.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
             this.txtsello.ForeColor = System.Drawing.Color.DarkRed;
-            this.txtsello.Location = new System.Drawing.Point(104, 432);
+            this.txtsello.Location = new System.Drawing.Point(104, 523);
             this.txtsello.Name = "txtsello";
             this.txtsello.Size = new System.Drawing.Size(194, 25);
             this.txtsello.TabIndex = 17;
@@ -396,7 +400,7 @@
             // 
             this.label15.AutoSize = true;
             this.label15.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label15.Location = new System.Drawing.Point(54, 435);
+            this.label15.Location = new System.Drawing.Point(54, 526);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(40, 20);
             this.label15.TabIndex = 34;
@@ -407,7 +411,7 @@
             this.btnGuardarFlete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
             this.btnGuardarFlete.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGuardarFlete.ForeColor = System.Drawing.Color.White;
-            this.btnGuardarFlete.Location = new System.Drawing.Point(433, 595);
+            this.btnGuardarFlete.Location = new System.Drawing.Point(433, 686);
             this.btnGuardarFlete.Name = "btnGuardarFlete";
             this.btnGuardarFlete.Size = new System.Drawing.Size(85, 32);
             this.btnGuardarFlete.TabIndex = 19;
@@ -419,7 +423,7 @@
             // 
             this.btnCancelar.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.Red;
-            this.btnCancelar.Location = new System.Drawing.Point(528, 595);
+            this.btnCancelar.Location = new System.Drawing.Point(528, 686);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.Size = new System.Drawing.Size(85, 32);
             this.btnCancelar.TabIndex = 20;
@@ -431,7 +435,7 @@
             // 
             this.label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label16.Font = new System.Drawing.Font("Arial Narrow", 1F);
-            this.label16.Location = new System.Drawing.Point(-43, 460);
+            this.label16.Location = new System.Drawing.Point(-43, 551);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(710, 10);
             this.label16.TabIndex = 38;
@@ -492,7 +496,7 @@
             // txtcomentarios
             // 
             this.txtcomentarios.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.txtcomentarios.Location = new System.Drawing.Point(96, 484);
+            this.txtcomentarios.Location = new System.Drawing.Point(96, 575);
             this.txtcomentarios.Name = "txtcomentarios";
             this.txtcomentarios.Size = new System.Drawing.Size(491, 96);
             this.txtcomentarios.TabIndex = 18;
@@ -502,7 +506,7 @@
             // 
             this.label20.AutoSize = true;
             this.label20.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label20.Location = new System.Drawing.Point(10, 470);
+            this.label20.Location = new System.Drawing.Point(10, 561);
             this.label20.Name = "label20";
             this.label20.Size = new System.Drawing.Size(83, 20);
             this.label20.TabIndex = 45;
@@ -512,7 +516,7 @@
             // 
             this.label21.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label21.Font = new System.Drawing.Font("Arial Narrow", 1F);
-            this.label21.Location = new System.Drawing.Point(-43, 585);
+            this.label21.Location = new System.Drawing.Point(-43, 676);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(710, 10);
             this.label21.TabIndex = 46;
@@ -521,7 +525,7 @@
             // 
             this.chkOperadorPendiente.AutoSize = true;
             this.chkOperadorPendiente.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkOperadorPendiente.Location = new System.Drawing.Point(33, 358);
+            this.chkOperadorPendiente.Location = new System.Drawing.Point(33, 449);
             this.chkOperadorPendiente.Name = "chkOperadorPendiente";
             this.chkOperadorPendiente.Size = new System.Drawing.Size(89, 24);
             this.chkOperadorPendiente.TabIndex = 14;
@@ -529,12 +533,54 @@
             this.chkOperadorPendiente.UseVisualStyleBackColor = true;
             this.chkOperadorPendiente.CheckedChanged += new System.EventHandler(this.chkOperadorPendiente_CheckedChanged);
             // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label22.Location = new System.Drawing.Point(40, 205);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(46, 20);
+            this.label22.TabIndex = 47;
+            this.label22.Text = "Carga:";
+            // 
+            // txtDireccionCarga
+            // 
+            this.txtDireccionCarga.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.txtDireccionCarga.Location = new System.Drawing.Point(103, 198);
+            this.txtDireccionCarga.Name = "txtDireccionCarga";
+            this.txtDireccionCarga.Size = new System.Drawing.Size(491, 44);
+            this.txtDireccionCarga.TabIndex = 48;
+            this.txtDireccionCarga.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.richTextBox2.Location = new System.Drawing.Point(103, 246);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(491, 44);
+            this.richTextBox2.TabIndex = 50;
+            this.richTextBox2.Text = "";
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label23.Location = new System.Drawing.Point(40, 253);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(46, 20);
+            this.label23.TabIndex = 49;
+            this.label23.Text = "Carga:";
+            // 
             // FrmRegistroViaje
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(625, 630);
+            this.ClientSize = new System.Drawing.Size(618, 726);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.txtDireccionCarga);
+            this.Controls.Add(this.label22);
             this.Controls.Add(this.chkOperadorPendiente);
             this.Controls.Add(this.label21);
             this.Controls.Add(this.label20);
@@ -633,5 +679,9 @@
         private System.Windows.Forms.Label label20;
         private System.Windows.Forms.Label label21;
         private System.Windows.Forms.CheckBox chkOperadorPendiente;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.RichTextBox txtDireccionCarga;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label23;
     }
 }
